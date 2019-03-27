3d2b73f1452fb903829767303767d513bdfb763f
Ravie <ravijkee@yandex.ru>
2019-03-27 13:56:54 +0300

    Use private modifier for IVector[]

--- src/matrix/DenseMatrix.java
+++ src/matrix/DenseMatrix.java
@@ -3,7 +3,8 @@ package matrix;
 public class DenseMatrix extends Matrix {
     public DenseMatrix(int row, int column) {
         super(row, column);
-        for (int i = 0; i < row; i++)
-            matrix[i] = new DenseVector(column);
     }
+
+    @Override
+    public IVector createVector(int column) { return new DenseVector(column); }
 }
--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -1,15 +1,18 @@
 package matrix;
 
 public abstract class Matrix implements IMatrix {
-    protected IVector[] matrix;
+    private IVector[] matrix;
     private int row, column;
 
     protected Matrix(int row, int column) {
         this.row = row;
         this.column = column;
         matrix = new IVector[row];
+        for (int i = 0; i < row; i++)
+            matrix[i] = createVector(column);
     }
 
+    public abstract IVector createVector(int column);
     public int getElem(int row, int column) { return matrix[row].getElem(column); }
     public void setElem(int row, int column, int elem) { matrix[row].setElem(column, elem); }
     public int getRowNumber() { return row; }
